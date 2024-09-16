3557072839bbec565a27cf44b03dd4cbed47395e
jack <obri.jack.02@gmail.com>
2024-09-16 12:27:04 +1000

    Working on auto-session

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 --[[
 
 =====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
+=====================================================================
 =====================================================================
 ========                                    .-----.          ========
 ========         .----------------------.   | === |          ========
@@ -20,68 +20,6 @@
 =====================================================================
 =====================================================================
 
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
 -- Set <space> as the leader key
@@ -94,8 +32,22 @@ vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = false
 
 -- neovide config
-vim.g.neovide_scroll_animation_length = 0
-vim.g.neovide_cursor_animation_length = 0
+if vim.g.neovide then
+  vim.g.neovide_scroll_animation_length = 0
+  vim.g.neovide_cursor_animation_length = 0
+  -- keymaps for zooming in and out in neovide.
+  local get_scaling_function = function(scale)
+    return function()
+      vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * scale
+    end
+  end
+
+  -- set initial scale to 0.8
+  get_scaling_function(0.8)()
+
+  vim.keymap.set({ 'n', 'i' }, '<C-->', get_scaling_function(0.95))
+  vim.keymap.set({ 'n', 'i' }, '<C-=>', get_scaling_function(1.05))
+end
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -194,19 +146,6 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
--- keymaps for zooming in and out in neovide.
-local get_scaling_function = function(scale)
-  return function()
-    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * scale
-  end
-end
-
--- set initial scale to 0.8
-get_scaling_function(0.8)()
-
-vim.keymap.set({ 'n', 'i' }, '<C-->', get_scaling_function(0.95))
-vim.keymap.set({ 'n', 'i' }, '<C-=>', get_scaling_function(1.05))
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -1,17 +1,21 @@
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
 return {
   {
     'rmagatti/auto-session',
     lazy = false,
     dependencies = {
-      --'nvim-telescope/telescope.nvim', -- Only needed if you want to use session lens
+      'nvim-telescope/telescope.nvim', -- Only needed if you want to use session lens
     },
   
     ---enables autocomplete for opts
     ---@module "auto-session"
     ---@type AutoSession.Config
     opts = {
-      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-      -- log_level = 'debug',
+      --suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      log_level = 'debug',
+      auto_restore_last_session = true,
+      --cwd_change_handling = true,
     }
   }
   
